4598654dcc5cf1704d5ca91b1c95888303ac03a1
yzw <zhenwang.yang@yintech.cn>
2023-12-18 13:13:23 +0800

    feat: 初始化

--- vite.config.ts
+++ vite.config.ts
@@ -61,6 +61,23 @@ export default (configEnv: ConfigEnv): UserConfigExport => {
           manualChunks: {
             vue: ["vue", "vue-router", "pinia"],
             element: ["element-plus", "@element-plus/icons-vue"]
+          },
+          // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
+          entryFileNames: "js/[name].[hash].js",
+          // 用于命名代码拆分时创建的共享块的输出命名
+          chunkFileNames: "js/[name].[hash].js",
+          // 用于输出静态资源的命名，[ext]表示文件扩展名
+          assetFileNames: (assetInfo: any) => {
+            const info = assetInfo.name.split(".")
+            let extType = info[info.length - 1]
+            if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)) {
+              extType = "media"
+            } else if (/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(assetInfo.name)) {
+              extType = "img"
+            } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
+              extType = "fonts"
+            }
+            return `${extType}/[name].[hash].[ext]`
           }
         }
       }
